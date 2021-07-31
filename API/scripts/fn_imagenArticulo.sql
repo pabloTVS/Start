ada38d0b7bd0e5aa03380eb2234107242caa1bbc
pabloTVS <pablo@truevaluesoft.com>
2021-07-31 11:58:25 +0200

    fn_imagen

--- API/scripts/fn_imagenArticulo.sql
+++ API/scripts/fn_imagenArticulo.sql
@@ -3,6 +3,6 @@ CREATE DEFINER=`startden_dekorativa`@`213.181.68.23` FUNCTION `fn_imagenArticulo
 ) RETURNS varchar(200) CHARSET latin1
 BEGIN 
 	DECLARE valor VARCHAR(200);
-	SET valor = (SELECT CONCAT('https://startdental.es/wp-content/uploads/2021/03/',substring_index(post_title,'.',1),'-100x100.jpg') Imagen FROM `wp_posts` WHERE post_parent = art ORDER BY ID DESC LIMIT 1);
+	SET valor = (SELECT CONCAT('https://startdental.es/wp-content/uploads/2021/03/',substring_index(post_title,'.',1),'-100x100.jpg') Imagen FROM `wp_posts` WHERE post_parent = art ORDER BY ID DESC LIMIT 1)
 	RETURN valor;
 END
