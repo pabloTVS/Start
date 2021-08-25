37108bd4ece84e3ffdcf67509522a74f16b4e29c
pabloTVS <pablo@truevaluesoft.com>
2021-08-25 19:12:26 +0200

    cambios clientes

--- src/app/pages/admin/Customers/clientes-list.component.ts
+++ src/app/pages/admin/Customers/clientes-list.component.ts
@@ -3,15 +3,12 @@ import { Component, OnInit, OnDestroy, AfterViewInit,ViewChild } from '@angular/
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { SpinnerOverlayService } from '@shared/services/spinner-overlay.service'
-import { MatDialog } from '@angular/material/dialog';
-// import { CustomersModalComponent } from '@pages/admin/components/modal/customers.modal/customers.modal.component'
 import { takeUntil } from 'rxjs/operators';
 
 import { CustomersService } from '../services/customers.service';
 import { AuthService } from '@app/pages/auth/auth.service';
-
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes-list.component.html',
@@ -24,10 +21,8 @@ export class CustomersListComponent implements AfterViewInit, OnInit, OnDestroy 
   DSCustomer = new MatTableDataSource();
 
   private destroy$ = new Subject<any>();
-
-  //codCom : number;
-  
-
+    
+//traígo los valores del Observable user.
   userValue = this.authSvc.userValue;
   codCom = this.userValue.commercial;
   role = this.userValue.role;
@@ -36,17 +31,15 @@ export class CustomersListComponent implements AfterViewInit, OnInit, OnDestroy 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator; 
 
-  constructor(private custSvc: CustomersService,
+  constructor(
+    private custSvc: CustomersService,
     private spinnerSvc: SpinnerOverlayService,
-    private dialog: MatDialog,
     private authSvc: AuthService) { }
 
   ngOnInit(): void {
     this.spinnerSvc.show();
-    this.custSvc.getAll(this.codCom,this.role).subscribe((customers) =>{
-      this.DSCustomer.data= customers;
-      this.spinnerSvc.hide();
-    })
+    this.custSvc.getAll(this.codCom,this.role).subscribe((customers) =>{this.DSCustomer.data= customers;});  
+    this.spinnerSvc.hide();
   }
   
   applyFilter(event: Event) {
@@ -62,22 +55,7 @@ export class CustomersListComponent implements AfterViewInit, OnInit, OnDestroy 
   {
       this.DSCustomer.data.length >0 ? this.spinnerSvc.hide() : this.spinnerSvc.show();
   }
- /*
-  onOpenModal(customer = {}): void {
-    // console.log('prod->', prod);
-     let dialogRef = this.dialog.open(CustomersModalComponent, {
-       height: '768px',
-       width: '1024px',
-       hasBackdrop: false,
-       data: { title: 'Clientes', customer },
-     });
-     dialogRef.afterClosed().subscribe(result => {
-       console.log(`Dialog result: ${result}`, typeof result);
-       // Update result after adding new user.
-       this.custSvc.getAll().subscribe((customer) => {this.DSCustomer.data = customer;});
-     });
-  }*/
- 
+
   onDelete(custId: number) :void{
     if (window.confirm('¿Estás seguro de borrar este cliente?.')) {
       this.custSvc
